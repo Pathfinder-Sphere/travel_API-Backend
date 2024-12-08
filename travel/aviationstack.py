03efeaae9609bb5e7be4c6fc3dab37c7baaf9f08
madefromjames <madefromjames@gmail.com>
2024-12-08 10:29:21 +0100

    save API response to response.json and comment out flight printing logic

--- travel/aviationstack.py
+++ travel/aviationstack.py
@@ -17,12 +17,15 @@ api_result = requests.get('https://api.aviationstack.com/v1/flights', params)
 api_response = api_result.json()
 print(api_response)
 
-for flight in api_response['results']:
-    if (flight['live']['is_ground'] is False):
-        print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
-            flight['airline']['name'],
-            flight['flight']['iata'],
-            flight['departure']['airport'],
-            flight['departure']['iata'],
-            flight['arrival']['airport'],
-            flight['arrival']['iata']))
+with open('response.json', 'w') as f:
+    f.write(str(api_response))
+
+# for flight in api_response['results']:
+#     if (flight['live']['is_ground'] is False):
+#         print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
+#             flight['airline']['name'],
+#             flight['flight']['iata'],
+#             flight['departure']['airport'],
+#             flight['departure']['iata'],
+#             flight['arrival']['airport'],
+#             flight['arrival']['iata']))
